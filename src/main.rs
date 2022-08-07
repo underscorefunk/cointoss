ca9282fae5ba1013f87630fa2f9a0301baab0b3b
John Funk <john@underscorefunk.com>
2022-08-07 17:52:11 -0500

    Working example

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@
 // @todo - Event data (use a closure to wrap and apply it)
 // @todo - Consider adding _uninitialized state
 
-#[derive(Debug, Clone)]
-enum State {
+// Each state variant should have it's config.
+// - On Enter
+// - On Event
 
+#[derive(Debug, Clone, PartialEq)]
+enum State {
     Idle,
     TossingCoin,
     Win,
@@ -47,9 +50,26 @@ impl Machine {
 
     pub fn send(&mut self, event: Event) {
         match self.state {
+
             State::Idle => match event {
                 Event::Toss => {
-                    self.state = State::TossingCoin
+
+                    // Transition state
+                    self.state = State::TossingCoin;
+
+                    // coinToss Action
+                    // Do on enter actions (imagine we invoke here)
+                    self.context.wins += 1;
+
+                    //Guarded transition
+                    if self.context.wins >= 3 {
+                        self.state = State::Win;
+                        return;
+                    }
+
+                    // Forced transition
+                    self.state = State::Idle;
+
                 },
                 _ => ()
             },
@@ -64,6 +84,11 @@ fn main() {
     println!("Coin Toss Game");
     let mut game = Machine::new();
     println!("{:?}", game);
+
     game.send( Event::Toss );
+    game.send( Event::Toss );
+    game.send( Event::Toss );
+    game.send( Event::Toss );
+
     println!("{:?}", game);
 }
