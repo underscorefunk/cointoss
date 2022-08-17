ed7809af62504f3e509e3471cc5f6e985c37c035
John Funk <john@underscorefunk.com>
2022-08-16 20:02:49 -0500

    Working initial state

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+#[derive(Debug, PartialEq)]
+enum State {
+    Idle,
+}
+
+#[derive(Debug)]
+struct Machine {}
+impl Machine {
+    pub fn state(&self) -> State {
+        State::Idle
+    }
+}
+
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::*;
+
+    #[test]
+    fn initial_machine() {
+        let coin_toss = Machine {};
+        assert_eq!(coin_toss.state(), State::Idle);
+    }
+}
