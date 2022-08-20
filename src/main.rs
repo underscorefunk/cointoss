f0638c14fd31cbe878c96ca0a7e75384d5c52bb6
John Funk <john@underscorefunk.com>
2022-08-20 11:12:57 -0500

    Document next steps

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,18 @@ async fn populate_stream(s: Sender<&str>) {
 
 #[tokio::main]
 async fn main() -> Result<(), Infallible> {
+    //
+    // Next steps
+    // ----------
+    // @todo - Find a way to have multiple async tasks pushing to the queue
+    //       - Find a way to continually consume the queue without blocking additions
+    //
+    //      The channel will become the event queue of a machine
+    //      The machine's event loop will process events or pause if there are no events
+    //      Other async tasks need to be able to ask the machine to do something, and then
+    //      that machine will take the list of things to do and try to do them.
+    //      Use a sandwich shop, front of house, and the kitchen as a domain example.
+
     // We create a sender and a single mutable receiver to process channel messages.
     // Only one single entity should be responsible for dealing with things on the queue.
     let (send, mut receive): (Sender<&str>, Receiver<&str>) = channel(10);
