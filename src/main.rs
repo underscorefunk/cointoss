7788f83e5bf18167f1225bf1bfbc44dd985609d9
John Funk <john@underscorefunk.com>
2022-08-20 11:08:17 -0500

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,15 @@ async fn populate_stream(s: Sender<&str>) {
 
 #[tokio::main]
 async fn main() -> Result<(), Infallible> {
+    // We create a sender and a single mutable receiver to process channel messages.
+    // Only one single entity should be responsible for dealing with things on the queue.
     let (send, mut receive): (Sender<&str>, Receiver<&str>) = channel(10);
 
+    // Add a message to the queue. Though this is async, we're awaiting here and there
+    // are no other async tasks so it will be forced to run to completion.
     populate_stream(send).await;
 
+    // We pluck the message off of the queue to confirm things worked as expected.
     if let Ok(message) = receive.recv().await {
         println!("{}", message);
     }
